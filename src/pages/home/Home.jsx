5cf6e972bf6449f2cb800a0646a93950d36ecb9d
Mohammadjavad Salehi <mjsalehi1381@gmail.com>
2023-07-04 21:59:39 +0330

    add sidebar and style of that

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
-import "./home.scss"
+import Sidebar from "../../components/sidebar/Sidebar";
+import "./home.scss";
 const Home = () => {
   return (
-    <div className='home'>
-        <h1 className="title">Home</h1>
+    <div className="home">
+              <Sidebar />
+      <div className="homeContainer">container</div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
